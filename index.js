445cb1ad4009eb0fd7f37610755f89f409d29fb4
geesssveee <1cgudimella@gmail.com>
2023-02-25 22:14:59 -0500

    creating logs

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const { format }= require('date-fns')
-const { v4: uuid} = require('uuid')
+const logEvents = require('./logEvents')
 
+const EventEmitter = require('events')
 
-console.log(format(new Date(),'yyyyMMdd\tHH:mm:ss'))
-console.log(uuid())
+class MyEmitter extends EventEmitter {};
+// Initialize object
+const myEmitter    =new MyEmitter()
+// add listener for the log event
+myEmitter.on('log',(msg)=>logEvents(msg))
+
+setTimeout(() => {
+    myEmitter.emit('log','Log event emitted!')
+}, 2000);
